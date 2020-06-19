8218c78a3685cdcf634d3e75be71f279b511be67
eatradish <sakiiily@aosc.io>
2020-06-19 22:57:22 +0800

    Podcast using templates

---
+++ scripts/shikwasa.js
@@ -0,0 +1,15 @@
+hexo.extend.tag.register('podplayer', function(args){
+    return `
+    <div class="podcast"></div>\
+    <script>\
+        const player = new Shikwasa({
+            container: () => document.querySelector('.podcast'),\
+            audio: {\
+                title: '${this.subtitle}',\
+                artist: '${this.author}',\
+                cover: '${this.image}',\
+                src: '${this.media}',\
+            },\
+        });\
+    </script>`
+});
